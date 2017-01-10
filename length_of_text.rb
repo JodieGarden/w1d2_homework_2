8ffbc1771184b01e35aed6f76edfd00b22193153
JodieGarden <jodie.garden@gmail.com>
2017-01-10 16:08:12 +0000

    fixed length_of_text

--- length_of_text.rb
+++ length_of_text.rb
@@ -1,8 +1,5 @@
-#def length_of_text(first, second)
-#  return "#{(first)}" + "#{second}".length
-#end
-def length_of_text (first, second)
-length_of_text= "Jodie Garden"
-length_of_text.length
+def length_of_text(words)
+return "words".length
 
-
+end
+puts words = ("Hi im jodie".length)
